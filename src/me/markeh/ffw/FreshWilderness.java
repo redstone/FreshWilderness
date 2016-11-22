2b058b64658b0bb737c6ed9f585834c7d8d445c9
Mark Hughes <m@rkhugh.es>
2016-11-22 16:09:02 +1100

    Add Towny integration

--- src/me/markeh/ffw/FreshWilderness.java
+++ src/me/markeh/ffw/FreshWilderness.java
@@ -20,6 +20,7 @@ import me.markeh.ffw.exceptions.IntegrationNotAddedException;
 import me.markeh.ffw.integrations.Ignition;
 import me.markeh.ffw.integrations.Integrations;
 import me.markeh.ffw.integrations.factions.FactionsIgnition;
+import me.markeh.ffw.integrations.towny.TownyIgnition;
 import me.markeh.ffw.regentask.RegenTask;
 import me.markeh.ffw.store.Config;
 import me.markeh.ffw.store.WildernessLog;
@@ -51,6 +52,7 @@ public class FreshWilderness extends JavaPlugin implements Listener {
 
 		// Add our integrations
 		Integrations.get().addIntegration(FactionsIgnition.get(), true);
+		Integrations.get().addIntegration(TownyIgnition.get(), true);
 		
 		this.getServer().getPluginManager().registerEvents(this, this);
 		
---
+++ src/me/markeh/ffw/integrations/towny/TownyIgnition.java
@@ -0,0 +1,33 @@
+package me.markeh.ffw.integrations.towny;
+
+import org.bukkit.Bukkit;
+
+import me.markeh.ffw.integrations.Engine;
+import me.markeh.ffw.integrations.Ignition;
+
+public class TownyIgnition  extends Ignition {
+
+	private static TownyIgnition i = new TownyIgnition();
+	public static TownyIgnition get() { return i; }
+	
+	public TownyIgnition() {
+		this.setPluginName("Towny");
+	}
+	
+	private TownyEngine engine = null;
+	
+	@Override
+	public Boolean isEnabled() {
+		return Bukkit.getPluginManager().isPluginEnabled("Towny");
+	}
+
+	@Override
+	public Engine getEngine() {
+		if (this.engine == null) {
+			this.engine = new TownyEngine();
+		}
+		
+		return this.engine;
+	}
+
+}
